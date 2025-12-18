18615d488456bc925ced3e911c1c78a086931bd7
Washu Ravele <washuravele75@gmail.com>
2025-12-18 16:46:35 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,55 @@
+import fs from "fs"
+import e from "express";
+import bodyParser from "body-parser";
+
+
+const app = e();
+const port = 4000;
+
+/*universities json*/
+const data1 = fs.readFileSync('./courses.json','utf-8');
+const universitiesCoursers = JSON.parse(data1);
+
+
+console.log(universitiesCoursers);
+
+//all uni coursers
+app.get("/universitiesCoursers",(req,res)=>{2
+   res.json(universitiesCoursers);
+});
+
+//get a specific uni  courses + facults
+app.get("/university/:uniName", (req, res) => {
+  const universityName = req.params.uniName;
+  
+  // Assuming universitiesCoursers is an array of objects
+  const foundUniversity = universitiesCoursers.filter(
+    (university) => university.name === universityName
+  );
+
+  res.json(foundUniversity);
+});
+
+//get courses with uni name and faculty
+app.get("/university/:uniName/:faculty", (req, res) => {
+  const universityName = req.params.uniName;
+  const universityFaculty = req.params.faculty;
+
+  // Assuming universitiesCoursers is an array of objects
+  const foundFaculty = universitiesCoursers.filter(
+    (university) =>
+      university.name === universityName &&
+      university.Faculty === universityFaculty // check case sensitivity
+  );
+
+  res.json(foundFaculty);
+});
+
+
+
+
+
+
+app.listen(port,()=>{
+    console.log(`Server running of http://localhost:${port}`);
+})
